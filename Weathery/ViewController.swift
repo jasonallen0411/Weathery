3a5057c677188773bb1a3c13c7b6b1054d316412
Jason Allen <jaallen@nossi.edu>
2019-06-20 01:44:45 -0500

    Initial Commit

---
+++ Weathery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Jason Allen on 6/20/19.
+//  Copyright © 2019 Jason Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
